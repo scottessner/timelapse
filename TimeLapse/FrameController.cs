9931efc98f7c3b8384f2ee74b0ceb4e4d7a0703e
scottessner <scott.essner@gmail.com>
2013-12-16 14:51:08 -0600

    Added upload to Frame.  Cleaned up controller

--- TimeLapse/FrameController.cs
+++ TimeLapse/FrameController.cs
@@ -13,58 +13,30 @@ namespace TimeLapse
     {
         public string SavePath { get; private set; }
         public int GrabFrequency { get; set; }
-        public string CameraURL { get; set; }
         public string UploadURL { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime StopTime { get; set; }
 
+        List<Frame> myFrames = new List<Frame>();
         GrabTimer timer;
-        WebUpdater cam;
         PictureUploader uploader;
-        IFrameSource frameSource = new UbiquitiAirCamFrameSource();
+        IFrameSource frameSource;
 
-        public FrameController()
+        public FrameController(IFrameSource FrameSource)
         {
-            timer = new GrabTimer(GrabFrequency);
-            cam = new WebUpdater(CameraURL);
-            uploader = new PictureUploader(UploadURL, 1);
+            this.frameSource = FrameSource;
         }
-        //private PictureUploader 
 
-        public Frame GrabImageFromCamera() 
+        public Frame GrabFromCamera() 
         {
             Frame thisFrame = frameSource.CaptureFrame();
-
-            //string fileName = "";
-            //Image grabbedImage = null;
-            //Frame im = null;
-            
-            ////File Name Construction
-            ////Round current time based on grab frequency
-            //DateTime dt = DateTime.Now;
-            //dt = dt.AddSeconds(-dt.Second % (GrabFrequency/1000));
-            //fileName = dt.ToString("yyyyMMdd-HHmmss") + ".jpg";
-
-            ////Grab Image using HTTP
-            //using( var client = new HttpClient())
-            //{
-            //    var response = client.GetAsync(CameraURL).Result;
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        grabbedImage = Image.FromStream(response.Content.ReadAsStreamAsync().Result);
-            //    }
-            //}
-
-            ////If we got an image back, create an ImageData object
-            //if (grabbedImage != null)
-            //{
-            //    //im = new Frame(grabbedImage, fileName);
-            //}
-
             return thisFrame;
         }
 
-        public void UploadImageToWebsite() { }
+        public bool UploadToServer(Frame frame, string URL) 
+        {
+            return frame.Upload(URL);
+        }
 
         public void SaveImageToFile() { }
 
