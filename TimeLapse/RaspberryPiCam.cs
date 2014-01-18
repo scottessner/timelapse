6cdeb6fcab7965fae1ffc8a8c6d49277dbf568ea
scottessner <scott.essner@gmail.com>
2014-01-17 19:16:59 -0600

    Testable Implementation of Rpi Cam

--- TimeLapse/RaspberryPiCam.cs
+++ TimeLapse/RaspberryPiCam.cs
@@ -2,11 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using System.IO;
 
 namespace TimeLapse
 {
     class RaspberryPiCam : ICamera
     {
 
+        public int UniqueID { get; private set; }
+
+        public RaspberryPiCam(int UniqueID)
+        {
+            this.UniqueID = UniqueID;
+        }
+
+        public Frame CaptureFrame()
+        {
+            Frame newFrame = new Frame();
+            newFrame.CaptureTime = DateTime.Now;
+            newFrame.CameraID = this.UniqueID;
+            newFrame.ImageBytes = CaptureImage();
+            if (newFrame.ImageBytes != null)
+                return newFrame;
+            else
+                return null;
+        }
+
+        private byte[] CaptureImage()
+        {
+            byte[] grabbedImage = null;
+
+            Process raspistill = new Process();
+            raspistill.StartInfo.FileName = "raspistill";
+            raspistill.StartInfo.Arguments = "";
+            raspistill.Start();
+            raspistill.WaitForExit();
+
+            MemoryStream ms = new MemoryStream();
+            raspistill.StandardOutput.BaseStream.CopyTo(ms);
+
+            grabbedImage = ms.ToArray();
+
+            return grabbedImage;
+
+        }
     }
 }
