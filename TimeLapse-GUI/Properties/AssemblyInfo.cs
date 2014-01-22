e5602fc5a5f2e9e0ada7294852aded792e75cc20
scottessner <scott.essner@gmail.com>
2014-01-22 12:08:58 -0600

    Renamed GUI Project

--- TimeLapse-GUI/Properties/AssemblyInfo.cs
+++ TimeLapse-GUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TimeLapse")]
+[assembly: AssemblyTitle("TimeLapse-GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TimeLapse")]
+[assembly: AssemblyProduct("TimeLapse-GUI")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
